2231fb662305aee1ca53369efcce44e5db0b3065
Fabio Duzaq <duzaq@live.com>
2024-11-19 15:22:33 +0000

    update port

--- app.js
+++ app.js
@@ -205,8 +205,8 @@ function callSendAPI(senderPsid, response) {
 }
 
 // listen for requests :)
-var listener = app.listen(36443, function () {
-  //var listener = app.listen(process.env.PORT, function () {
+//var listener = app.listen(36443, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
